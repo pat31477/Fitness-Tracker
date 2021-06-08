275ffcbfe510a22312994cd6ca12fe4a34d33d5c
Pat31477 <pat31477@yahoo.com>
2021-06-07 20:33:27 -0400

    first commit of apiroutes file

---
+++ routes/apiroutes.js
@@ -0,0 +1,39 @@
+const db = require("../models");
+
+module.exports = function(app) {
+    // App.get to retrieve data for the workout
+    app.get("/api/workouts", (req, res) => {
+        db.Workout.find({}).then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+    })
+    // App.get to retrieve the data for the range
+    app.get("/api/workouts/range", ({}, res) => {
+      db.Workout.find({}).then((dbWorkout) => {
+        res.json(dbWorkout);
+      }).catch(err => {
+        res.status(400).json(err);
+      });
+    });
+    // App.post to submit newly completed workouts
+    app.post("/api/workouts/", (req, res) => {
+        db.Workout.create(req.body).then((dbWorkout) => {
+          res.json(dbWorkout);
+        }).catch(err => {
+            res.status(400).json(err);
+          });
+      });
+      // App.put to update the workouts by MongoDB _id value as well as update the exercise body
+      app.put("/api/workouts/:id", (req, res) => {
+        db.Workout.findByIdAndUpdate(
+          { _id: req.params.id }, { exercises: req.body }
+        ).then((dbWorkout) => {
+          res.json(dbWorkout);
+        }).catch(err => {
+          res.status(400).json(err);
+        });
+    });
+};
