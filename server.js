3a7a790a2ee2a989d6a592cc7d6302b0e5b85fe0
Pat31477 <pat31477@yahoo.com>
2021-06-08 10:02:11 -0400

    testing heroku again

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnesstracker", {
   useNewUrlParser: true,
-  useFindAndModify: False
+  useFindAndModify: false
 });
 
 require("./routes/htmlroutes")(app);
