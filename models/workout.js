08f111c3e2233b7f21d42f46c5a148b7fd17128b
Pat31477 <pat31477@yahoo.com>
2021-06-08 16:17:18 -0400

    deploying

--- models/workout.js
+++ models/workout.js
@@ -38,6 +38,6 @@ const WorkoutSchema = new Schema({
     ]
   });
   
-  const Workout = mongoose.model("workout", WorkoutSchema);
+  const Workout = mongoose.model("workouts", WorkoutSchema);
   
   module.exports = Workout;
